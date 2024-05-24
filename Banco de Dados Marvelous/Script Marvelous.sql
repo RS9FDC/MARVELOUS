f502e4a7cc85827921e13aab967cacf944cbf223
RS9FDC <ruan.santos@sptech.school>
2024-05-23 23:00:12 -0300

    Atualização do banco + Site institucional quase pronto

--- Banco de Dados Marvelous/Script Marvelous.sql
+++ Banco de Dados Marvelous/Script Marvelous.sql
@@ -2,16 +2,32 @@ create database Marvelous;
 
 use Marvelous;
 
-create table usuario (
-idUsuario int primary key,
+create table usuario(
+id int primary key auto_increment,
 nome char(10),
 email char(50),
 senha char(15)
 ); 
 
-INSERT INTO usuario VALUES
-(1, 'rato', 'vaca', 12345);
+CREATE TABLE aviso (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	titulo VARCHAR(100),
+	descricao VARCHAR(150),
+	fk_usuario INT,
+	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+);
 
 select * from usuario;
 
+insert into usuario values
+(default,'rato','sapo',123);
+
+insert into aviso values 
+(1, 'sapo', 'dodoizito', 1);
+
+select * from usuario join aviso on fk_usuario = usuario.id;
+
 truncate table usuario;
+
+drop database Marvelous;
+
